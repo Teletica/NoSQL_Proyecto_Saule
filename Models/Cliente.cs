32b996cbd89c930041ebd63697cf2c348c85625c
Josue Loria <jmiranda90400@ufide.ac.cr>
2024-12-15 18:22:06 -0600

    Mapeo de cliente
    Estoy teniendo unos problemas con el push

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,41 @@ namespace NoSQL_Proyecto_Saule.Models
 {
     public class Cliente
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required]
+        [BsonElement("nombreCliente")]
+        public string NombreCliente { get; set; }
+
+        [Required]
+        [BsonElement("apellidoUnoCliente")]
+        public string ApellidoUnoCliente { get; set; }
+
+        [BsonElement("apellidoDosCliente")]
+        public string ApellidoDosCliente { get; set; }
+
+        [Required]
+        [BsonElement("cedulaCliente")]
+        public string CedulaCliente { get; set; }
+
+        [Required]
+        [BsonElement("contactoCliente")]
+        public ContactoCliente contactoCliente { get; set; }
+
+        public class ContactoCliente
+        {
+            [Required]
+            [BsonElement("numTelefonoUnoCliente")]
+            public string NumTelefonoUnoCliente { get; set; }
+
+            [BsonElement("numTelefonoDosCliente")]
+            public string NumTelefonoDosCliente { get; set; }
+
+            [Required]
+            [BsonElement("correoCliente")]
+            public string CorreoCliente { get; set; }
+        }
     }
 }
